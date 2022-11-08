7c3d4e7c07abc7761ed611eeae49d44048915cac
lukacrnogorac <lcrnog97gmail.com>
2022-11-08 16:42:28 +0100

    WIP

---
+++ lib/ui/widgets/creators/comics.dart
@@ -0,0 +1,23 @@
+import 'package:d_reader_flutter/ui/widgets/creators/comic_card.dart';
+import 'package:flutter/material.dart';
+
+class CreatorComicsTab extends StatelessWidget {
+  const CreatorComicsTab({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 8,
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) {
+        return Container(
+          margin: const EdgeInsets.only(top: 24),
+          child: CreatorComicCard(
+            index: index,
+          ),
+        );
+      },
+    );
+  }
+}

1070be4b757c62bcfae04c31e53ece853767e0ea
lukacrnogorac <lcrnog97gmail.com>
2022-12-06 08:54:35 +0100

    minor changes

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -3,7 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 abstract class Config {
   static final walletPrivateKey =
       dotenv.get('private_key', fallback: 'Key is missing');
-  static final API_URL = dotenv.get('API_URL',
+  static final apiUrl = dotenv.get('API_URL',
       fallback:
           'https://d-reader-backend-dev.herokuapp.com'); //'http://10.0.2.2:3005'
   static final jwtToken = dotenv.get('JWT'); // testing purpose
--- lib/core/services/api_service.dart
+++ lib/core/services/api_service.dart
@@ -4,11 +4,11 @@ import 'package:d_reader_flutter/config/config.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static final String API_URL = Config.API_URL;
+  static final String apiUrl = Config.apiUrl;
 
   static Future<String?> apiCallGet(String path,
       {bool includeAuthHeader = true}) async {
-    Uri uri = Uri.parse('$API_URL$path');
+    Uri uri = Uri.parse('$apiUrl$path');
     http.Response response = await http.get(uri,
         headers: includeAuthHeader
             ? {HttpHeaders.authorizationHeader: 'Bearer ${Config.jwtToken}'}
@@ -21,7 +21,7 @@ class ApiService {
   }
 
   static Future<void> apiCallPatch(String path) async {
-    Uri uri = Uri.parse('$API_URL$path');
+    Uri uri = Uri.parse('$apiUrl$path');
     http.Response response = await http.patch(
       uri,
       headers: {HttpHeaders.authorizationHeader: 'Bearer ${Config.jwtToken}'},

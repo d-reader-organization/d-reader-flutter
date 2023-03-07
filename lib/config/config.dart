134377019d8ec60a145a47d5ed1b0b58c4eac98a
d-reader-luka <125265091+d-reader-luka@users.noreply.github.com>
2023-03-07 11:12:59 +0100

    Dev (#4)
    * changes according to the backend changes
    
    * Feat/mint transaction (#1)
    
    * mint transaction WIP
    
    * update packages
    
    * mint transaction, fix some deprecated dependencies
    
    * update url launcher
    
    * minor cards adjustments
    
    * Flutter upgrade & project cleanup
    
    * Mobile Changes - UI, Refactor, Features (#2)
    
    * is verified creator, minor ui changes
    
    * home screen - comic card changes
    
    * home screen - comic issue card fixes
    
    * home screen - creators and floor price change
    
    * total volume try to double parse
    
    * update models
    
    * comic slug screen fixes
    
    * remove toggle from view counter
    
    * comic issue details fixes
    
    * creator details fixes
    
    * discover screen fix issues
    
    * create results wrapper and use it on discover results page
    
    * rename getcomicissue
    
    * Remove Selectable comic issue listed items row
    
    * Details scaffold cleanup
    Extract to separate widgets
    
    * rename info section headings, remove mint functionality for now
    
    * create candy machine and receipt model. create candy machine repository and provider
    
    * use timeago for date format, make issue pages to work according to the backend changes
    
    * comic issue details fetch candy machine and listed data

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -2,8 +2,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 abstract class Config {
   static final apiUrl = dotenv.get('API_URL',
-      fallback:
-          'https://d-reader-backend-dev.herokuapp.com'); //'http://10.0.2.2:3005'
+      fallback: 'https://api.dev.dreader.io/'); //'http://10.0.2.2:3005'
   static final jwtToken = dotenv.get('JWT'); // testing purpose
   static final String candyMachineAddress = dotenv.get('candyMachineAddress');
   static final String solanaCluster =

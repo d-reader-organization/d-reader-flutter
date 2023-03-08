c2357946ca2588b4502e891435745307eb1c3dc5
d-reader-luka <luka.crnogorac@dreader.io>
2023-03-08 12:01:15 +0100

    create animated appbar widget

--- lib/ui/widgets/common/app_bar_without_logo.dart
+++ lib/ui/widgets/common/app_bar_without_logo.dart
@@ -36,3 +36,44 @@ class AppBarWithoutLogo extends StatelessWidget {
     );
   }
 }
+
+class AnimatedAppBar extends StatelessWidget {
+  final Animation<Color?> animation;
+  final String? title;
+  final List<Widget> actions;
+  const AnimatedAppBar({
+    super.key,
+    required this.animation,
+    this.title,
+    this.actions = const [
+      Icon(
+        Icons.more_horiz_outlined,
+        size: 16,
+        color: Colors.white,
+      ),
+    ],
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+        animation: animation,
+        builder: (context, child) {
+          return AppBar(
+            title: title != null
+                ? Text(
+                    '$title',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  )
+                : null,
+            centerTitle: true,
+            actions: actions,
+            backgroundColor: animation.value,
+            shadowColor: animation.value,
+          );
+        });
+  }
+}

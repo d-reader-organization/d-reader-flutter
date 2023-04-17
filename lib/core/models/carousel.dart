30d1e2f9a5edb60e0cd6c09816f4b95d546da377
d-reader-luka <luka.crnogorac@dreader.io>
2023-04-17 13:30:06 +0200

    carousel changes and on tap action

--- lib/core/models/carousel.dart
+++ lib/core/models/carousel.dart
@@ -1,14 +1,18 @@
 class CarouselModel {
   final int id;
-  final String image;
-  final String title;
-  final String subtitle;
+  final String image, title, subtitle;
+  final int? comicIssueId;
+  final String? comicSlug, creatorSlug, externalLink;
 
   CarouselModel({
     required this.id,
     required this.image,
     required this.title,
     required this.subtitle,
+    this.externalLink,
+    this.comicIssueId,
+    this.comicSlug,
+    this.creatorSlug,
   });
 
   factory CarouselModel.fromJson(dynamic data) {
@@ -17,6 +21,10 @@ class CarouselModel {
       image: data['image'],
       title: data['title'] ?? '',
       subtitle: data['subtitle'] ?? '',
+      externalLink: data['externalLink'] ?? '',
+      comicIssueId: data['comicIssueId'],
+      comicSlug: data['comicSlug'],
+      creatorSlug: data['creatorSlug'],
     );
   }
 }
--- lib/ui/widgets/common/carousel.dart
+++ lib/ui/widgets/common/carousel.dart
@@ -2,6 +2,11 @@ import 'package:carousel_slider/carousel_slider.dart';
 import 'package:d_reader_flutter/core/models/carousel.dart';
 import 'package:d_reader_flutter/core/providers/carousel_provider.dart';
 import 'package:d_reader_flutter/ui/shared/app_colors.dart';
+import 'package:d_reader_flutter/ui/utils/launch_external_url.dart';
+import 'package:d_reader_flutter/ui/utils/screen_navigation.dart';
+import 'package:d_reader_flutter/ui/views/comic_details.dart';
+import 'package:d_reader_flutter/ui/views/comic_issue_details.dart';
+import 'package:d_reader_flutter/ui/views/creators/creator_details.dart';
 import 'package:d_reader_flutter/ui/widgets/common/cached_image_bg_placeholder.dart';
 import 'package:d_reader_flutter/ui/widgets/common/cards/skeleton_card.dart';
 import 'package:flutter/material.dart';
@@ -28,49 +33,65 @@ class Carousel extends ConsumerWidget {
           ),
           items: data
               .map(
-                (carouselItem) => Stack(
-                  children: [
-                    CachedImageBgPlaceholder(
-                      height: 266,
-                      imageUrl: carouselItem.image,
-                      cacheKey: '${carouselItem.id}${carouselItem.title}',
-                      foregroundDecoration: BoxDecoration(
-                        gradient: const LinearGradient(
-                          colors: [
-                            ColorPalette.boxBackground200,
-                            Colors.transparent,
+                (carouselItem) => GestureDetector(
+                  onTap: () {
+                    if (carouselItem.externalLink != null) {
+                      openUrl(carouselItem.externalLink!);
+                    } else if (carouselItem.comicSlug != null) {
+                      return nextScreenPush(
+                          context, ComicDetails(slug: carouselItem.comicSlug!));
+                    } else if (carouselItem.comicIssueId != null) {
+                      return nextScreenPush(context,
+                          ComicIssueDetails(id: carouselItem.comicIssueId!));
+                    } else if (carouselItem.creatorSlug != null) {
+                      return nextScreenPush(context,
+                          CreatorDetailsView(slug: carouselItem.creatorSlug!));
+                    }
+                  },
+                  child: Stack(
+                    children: [
+                      CachedImageBgPlaceholder(
+                        height: 266,
+                        imageUrl: carouselItem.image,
+                        cacheKey: '${carouselItem.id}${carouselItem.title}',
+                        foregroundDecoration: BoxDecoration(
+                          gradient: const LinearGradient(
+                            colors: [
+                              ColorPalette.boxBackground200,
+                              Colors.transparent,
+                            ],
+                            begin: Alignment.bottomCenter,
+                            end: Alignment.topCenter,
+                            stops: [0, 0.8],
+                          ),
+                          borderRadius: BorderRadius.circular(
+                            16,
+                          ),
+                        ),
+                      ),
+                      Positioned.fill(
+                        bottom: 20,
+                        left: 16,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              carouselItem.subtitle,
+                              style: textTheme.bodySmall,
+                            ),
+                            const SizedBox(
+                              height: 8,
+                            ),
+                            Text(
+                              carouselItem.title,
+                              style: textTheme.headlineLarge,
+                            ),
                           ],
-                          begin: Alignment.bottomCenter,
-                          end: Alignment.topCenter,
-                          stops: [0, 0.8],
-                        ),
-                        borderRadius: BorderRadius.circular(
-                          16,
                         ),
                       ),
-                    ),
-                    Positioned.fill(
-                      bottom: 20,
-                      left: 16,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          Text(
-                            carouselItem.subtitle,
-                            style: textTheme.bodySmall,
-                          ),
-                          const SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            carouselItem.title,
-                            style: textTheme.headlineLarge,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               )
               .toList(),

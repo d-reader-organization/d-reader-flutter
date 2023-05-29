0ec6d8b84d652b5b7bd017c20ddc8478986bcd2e
d-reader-luka <luka.crnogorac@dreader.io>
2023-05-29 13:16:24 +0200

    creator details page changes

--- lib/core/models/creator.dart
+++ lib/core/models/creator.dart
@@ -1,22 +1,30 @@
 class CreatorModel {
   final int? id;
-  final String slug;
-  final String name;
-  final String avatar;
-  final String banner;
-  final String description;
+  final String avatar,
+      banner,
+      description,
+      instagram,
+      lynkfire,
+      name,
+      slug,
+      twitter,
+      website;
   final bool isVerified;
   final CreatorStats? stats;
   final CreatorMyStats? myStats;
 
   CreatorModel({
     this.id,
-    required this.slug,
-    required this.name,
     required this.avatar,
     required this.banner,
     required this.description,
+    required this.instagram,
     required this.isVerified,
+    required this.lynkfire,
+    required this.name,
+    required this.slug,
+    required this.twitter,
+    required this.website,
     this.stats,
     this.myStats,
   });
@@ -29,7 +37,11 @@ class CreatorModel {
       avatar: json['avatar'],
       banner: json['banner'] ?? '',
       description: json['description'] ?? '',
+      instagram: json['instagram'] ?? '',
       isVerified: json['isVerified'],
+      lynkfire: json['lynkfire'] ?? '',
+      twitter: json['twitter'] ?? '',
+      website: json['website'] ?? '',
       stats:
           json['stats'] != null ? CreatorStats.fromJson(json['stats']) : null,
       myStats: json['myStats'] != null

388927f43d8d017eac529f6bb5e8a13e670d4538
d-reader-luka <luka.crnogorac@dreader.io>
2023-05-31 11:04:44 +0200

    use dio client for comic issue repository and use repo from provider instead of ioc container

--- lib/core/providers/comic_issue_provider.dart
+++ lib/core/providers/comic_issue_provider.dart
@@ -1,41 +1,52 @@
 import 'package:d_reader_flutter/core/models/comic_issue.dart';
 import 'package:d_reader_flutter/core/models/page_model.dart';
 import 'package:d_reader_flutter/core/notifiers/pagination_notifier.dart';
+import 'package:d_reader_flutter/core/providers/dio_provider.dart';
 import 'package:d_reader_flutter/core/repositories/comic_issues/comic_issue_repository_impl.dart';
 import 'package:d_reader_flutter/core/states/pagination_state.dart';
-import 'package:d_reader_flutter/ioc.dart';
 import 'package:d_reader_flutter/ui/utils/append_default_query_string.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
+final comicIssueRepository = Provider<ComicIssueRepositoryImpl>(
+  (ref) {
+    return ComicIssueRepositoryImpl(
+      client: ref.watch(
+        dioProvider(
+          null,
+        ),
+      ),
+    );
+  },
+);
+
 final comicIssuesProvider = FutureProvider.autoDispose
     .family<List<ComicIssueModel>, String?>((ref, queryString) async {
-  return await IoCContainer.resolveContainer<ComicIssueRepositoryImpl>()
-      .getComicIssues(
-          queryString: queryString ?? appendDefaultQuery(queryString));
+  return ref.read(comicIssueRepository).getComicIssues(
+        queryString: queryString ??
+            appendDefaultQuery(
+              queryString,
+            ),
+      );
 });
 
 final comicIssueDetailsProvider =
     FutureProvider.autoDispose.family<ComicIssueModel?, int>((ref, id) {
-  return IoCContainer.resolveContainer<ComicIssueRepositoryImpl>()
-      .getComicIssue(id);
+  return ref.read(comicIssueRepository).getComicIssue(id);
 });
 
 final comicIssuePagesProvider =
     FutureProvider.family<List<PageModel>, int>((ref, id) async {
-  return await IoCContainer.resolveContainer<ComicIssueRepositoryImpl>()
-      .getComicIssuePages(id);
+  return ref.read(comicIssueRepository).getComicIssuePages(id);
 });
 
 final rateComicIssueProvider =
     FutureProvider.autoDispose.family<dynamic, dynamic>(
   (ref, input) async {
     if (input['id'] != null) {
-      final result =
-          await IoCContainer.resolveContainer<ComicIssueRepositoryImpl>()
-              .rateIssue(
-        id: input['id'],
-        rating: input['rating'],
-      );
+      final result = await ref.read(comicIssueRepository).rateIssue(
+            id: input['id'],
+            rating: input['rating'],
+          );
 
       return result;
     }
@@ -46,21 +57,9 @@ final paginatedIssuesProvider = StateNotifierProvider.autoDispose.family<
     PaginationNotifier<ComicIssueModel>,
     PaginationState<ComicIssueModel>,
     String?>((ref, query) {
-  final fetch =
-      IoCContainer.resolveContainer<ComicIssueRepositoryImpl>().getComicIssues;
+  final fetch = ref.read(comicIssueRepository).getComicIssues;
   return PaginationNotifier<ComicIssueModel>(
     fetch: fetch,
     query: query,
   )..init();
 });
-
-class ComicIssueDetailState {
-  const ComicIssueDetailState({
-    required this.selectedNftsCount,
-  });
-  final int selectedNftsCount;
-
-  ComicIssueDetailState copyWith({required int selectedNftsCount}) {
-    return ComicIssueDetailState(selectedNftsCount: selectedNftsCount);
-  }
-}
--- lib/ioc.dart
+++ lib/ioc.dart
@@ -2,7 +2,6 @@ import 'package:d_reader_flutter/core/repositories/auction_house/repository_impl
 import 'package:d_reader_flutter/core/repositories/auth/auth_repository_impl.dart';
 import 'package:d_reader_flutter/core/repositories/candy_machine/repository_implementation.dart';
 import 'package:d_reader_flutter/core/repositories/carousel/carousel_repository_impl.dart';
-import 'package:d_reader_flutter/core/repositories/comic_issues/comic_issue_repository_impl.dart';
 import 'package:d_reader_flutter/core/repositories/creator/creator_repository_impl.dart';
 import 'package:d_reader_flutter/core/repositories/genre/genre_repository_impl.dart';
 import 'package:d_reader_flutter/core/repositories/nft/repository_impl.dart';
@@ -15,7 +14,6 @@ abstract class IoCContainer {
     KiwiContainer container = KiwiContainer();
     container.registerSingleton((container) => AuthRepositoryImpl());
     container.registerSingleton((container) => CarouselRepositoryImpl());
-    container.registerSingleton((container) => ComicIssueRepositoryImpl());
     container.registerSingleton((container) => CreatorRepositoryImpl());
     container.registerSingleton((container) => GenreRepositoryImpl());
     container.registerSingleton((container) => ApiService());

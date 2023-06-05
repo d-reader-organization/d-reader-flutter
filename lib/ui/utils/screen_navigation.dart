5004cb89c07c4f7e80064664b4cf8b1cbc1143c7
Jean Paul Jacquot <jjacquot@admin-withingss-MacBook-Pro-2.local>
2023-06-05 11:49:09 +0200

    Add fade transition on navigation page

--- lib/ui/utils/screen_navigation.dart
+++ lib/ui/utils/screen_navigation.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void nextScreenPush(BuildContext context, Widget page) {
-  Navigator.push(context, MaterialPageRoute(builder: (context) => page));
+  Navigator.of(context).push(PageRouteBuilder(
+      pageBuilder: (BuildContext context, _, __) => page,
+      transitionsBuilder: (_, Animation<double> animation, __, Widget child) {
+        return FadeTransition(opacity: animation, child: child);
+      }));
 }
 
 void nextScreenCloseOthers(BuildContext context, Widget page) {

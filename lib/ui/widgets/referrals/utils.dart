5d14b928b2a46e6856963c3d84db6a5f03d5bfb6
d-reader-luka <luka.crnogorac@dreader.io>
2024-02-19 12:56:48 +0100

    referrals screen

--- lib/ui/widgets/referrals/utils.dart
+++ lib/ui/widgets/referrals/utils.dart
@@ -1,55 +1,20 @@
-import 'package:d_reader_flutter/core/models/user.dart';
-import 'package:d_reader_flutter/core/notifiers/environment_notifier.dart';
-import 'package:d_reader_flutter/core/providers/common_text_controller_provider.dart';
-import 'package:d_reader_flutter/core/providers/global_provider.dart';
-import 'package:d_reader_flutter/core/providers/user/user_provider.dart';
-import 'package:d_reader_flutter/core/states/environment_state.dart';
+import 'package:d_reader_flutter/core/providers/settings/referral.dart';
 import 'package:d_reader_flutter/ui/shared/app_colors.dart';
 import 'package:d_reader_flutter/ui/utils/show_snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 handleSave(BuildContext context, WidgetRef ref, String referrer) async {
-  final notifier = ref.read(globalStateProvider.notifier);
-  notifier.update(
-    (state) => state.copyWith(
-      isLoading: true,
-    ),
-  );
-  final currentUser = ref.read(environmentProvider).user;
-  dynamic updateResult;
-  if (currentUser != null) {
-    updateResult = await ref.read(userRepositoryProvider).updateUser(
-          UpdateUserPayload(
-            id: currentUser.id,
-            referrer: referrer,
-          ),
-        );
-  }
-
-  notifier.update(
-    (state) => state.copyWith(
-      isLoading: false,
-    ),
-  );
-  ref.invalidate(myUserProvider);
-  final result = await ref.read(myUserProvider.future);
-  ref.read(environmentProvider.notifier).updateEnvironmentState(
-        EnvironmentStateUpdateInput(
-          user: result,
-        ),
+  await ref.read(referralControllerProvider.notifier).handleSave(
+        referrer: referrer,
+        callback: (result) {
+          showSnackBar(
+            context: context,
+            text: result is String ? result : 'Referral code claimed',
+            backgroundColor: result is String
+                ? ColorPalette.dReaderRed
+                : ColorPalette.dReaderGreen,
+          );
+        },
       );
-
-  if (context.mounted) {
-    ref.read(commonTextEditingController).clear();
-    ref.read(commonTextValue.notifier).state = '';
-
-    showSnackBar(
-      context: context,
-      text: updateResult is String ? updateResult : 'Referral code claimed',
-      backgroundColor: updateResult is String
-          ? ColorPalette.dReaderRed
-          : ColorPalette.dReaderGreen,
-    );
-  }
 }

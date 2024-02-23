faa382bff3a9385c725a6a8efb4c0ddfae78d957
d-reader-luka <luka.crnogorac@dreader.io>
2024-02-23 13:57:11 +0100

    organize widgets of comic details

---
+++ lib/ui/widgets/comic_issues/details/tabs/about/expandable_container.dart
@@ -0,0 +1,167 @@
+import 'package:d_reader_flutter/constants/constants.dart';
+import 'package:d_reader_flutter/core/models/candy_machine_group.dart';
+import 'package:d_reader_flutter/core/providers/candy_machine_provider.dart';
+import 'package:d_reader_flutter/core/providers/comic_issue/provider.dart';
+import 'package:d_reader_flutter/ui/shared/app_colors.dart';
+import 'package:d_reader_flutter/ui/utils/formatter.dart';
+import 'package:d_reader_flutter/ui/widgets/common/solana_price.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+String mintNumbersText({required int itemsMinted, required int totalSupply}) {
+  return '${itemsMinted > totalSupply ? totalSupply : itemsMinted}/$totalSupply';
+}
+
+class ExpandableContainer extends ConsumerWidget {
+  final CandyMachineGroupModel candyMachineGroup;
+  final int totalSupply;
+  const ExpandableContainer({
+    super.key,
+    required this.candyMachineGroup,
+    required this.totalSupply,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final textTheme = Theme.of(context).textTheme;
+    final isFutureMint = candyMachineGroup.startDate != null &&
+        candyMachineGroup.startDate!.isAfter(DateTime.now());
+    final candyMachineState = ref.watch(candyMachineStateProvider);
+    return GestureDetector(
+      onTap: () {
+        ref.read(expandedCandyMachineGroup.notifier).update((state) =>
+            state != candyMachineGroup.label ? candyMachineGroup.label : '');
+      },
+      child: AnimatedContainer(
+        height: ref.watch(expandedCandyMachineGroup) == candyMachineGroup.label
+            ? 108
+            : 46,
+        duration: const Duration(milliseconds: 150),
+        margin: const EdgeInsets.only(bottom: 8),
+        child: _DecoratedContainer(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        candyMachineGroup.displayLabel,
+                        style: textTheme.titleMedium,
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      candyMachineGroup.isActive
+                          ? const CircleAvatar(
+                              backgroundColor: ColorPalette.dReaderYellow100,
+                              radius: 6,
+                            )
+                          : const SizedBox(),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      Text(
+                        candyMachineGroup.isActive
+                            ? 'Live'
+                            : isFutureMint
+                                ? 'Starts in ${Formatter.formatDateInRelative(candyMachineGroup.startDate)}'
+                                : 'Ended',
+                        style: textTheme.titleMedium?.copyWith(
+                          color: candyMachineGroup.isActive
+                              ? ColorPalette.dReaderYellow100
+                              : isFutureMint
+                                  ? ColorPalette.dReaderYellow300
+                                  : ColorPalette.greyscale200,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SolanaPrice(
+                    price: candyMachineGroup.mintPrice > 0
+                        ? Formatter.formatPriceWithSignificant(
+                            candyMachineGroup.mintPrice.round(),
+                          )
+                        : null,
+                  )
+                ],
+              ),
+              Column(
+                children: [
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  LinearProgressIndicator(
+                    backgroundColor: ColorPalette.greyscale400,
+                    minHeight: 8,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                      candyMachineGroup.isActive
+                          ? ColorPalette.dReaderYellow100
+                          : ColorPalette.greyscale200,
+                    ),
+                    value: candyMachineGroup.itemsMinted /
+                        candyMachineGroup.supply,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'You minted: ${candyMachineGroup.wallet?.itemsMinted ?? 0}/${candyMachineGroup.wallet?.supply ?? '∞'}',
+                        style: textTheme.bodySmall?.copyWith(
+                          color: ColorPalette.greyscale100,
+                        ),
+                      ),
+                      Text(
+                        candyMachineGroup.label == publicGroupLabel
+                            ? mintNumbersText(
+                                itemsMinted: candyMachineState?.itemsMinted ??
+                                    candyMachineGroup.itemsMinted,
+                                totalSupply: candyMachineState?.supply ??
+                                    candyMachineGroup.supply)
+                            : mintNumbersText(
+                                itemsMinted: candyMachineGroup.itemsMinted,
+                                totalSupply: candyMachineGroup.supply,
+                              ),
+                        style: textTheme.bodySmall?.copyWith(
+                          color: ColorPalette.greyscale100,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _DecoratedContainer extends StatelessWidget {
+  final Widget child;
+  const _DecoratedContainer({
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      physics: const NeverScrollableScrollPhysics(),
+      child: Container(
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: ColorPalette.greyscale500,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: child,
+      ),
+    );
+  }
+}
--- lib/ui/widgets/comic_issues/details/tabs/listings/filters.dart
+++ lib/ui/widgets/comic_issues/details/tabs/listings/filters.dart
@@ -1,67 +1,7 @@
-import 'package:d_reader_flutter/core/models/comic_issue.dart';
-import 'package:d_reader_flutter/core/notifiers/listings_notifier.dart';
-import 'package:d_reader_flutter/core/providers/auction_house_provider.dart';
 import 'package:d_reader_flutter/ui/shared/app_colors.dart';
-import 'package:d_reader_flutter/ui/utils/formatter.dart';
-import 'package:d_reader_flutter/ui/widgets/comic_issues/details/listed_items.dart';
-import 'package:d_reader_flutter/ui/widgets/common/skeleton_row.dart';
-import 'package:d_reader_flutter/ui/widgets/common/stats_info.dart';
-import 'package:d_reader_flutter/ui/widgets/discover/common/on_going_bottom.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:solana/solana.dart' show lamportsPerSol;
-
-class IssueListings extends ConsumerWidget {
-  final ComicIssueModel issue;
-  const IssueListings({
-    super.key,
-    required this.issue,
-  });
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    return NotificationListener(
-      onNotification: (notification) {
-        if (notification is ScrollNotification) {
-          double maxScroll = notification.metrics.maxScrollExtent;
-          double currentScroll = notification.metrics.pixels;
-          double delta = MediaQuery.sizeOf(context).width * 0.2;
-          if (maxScroll - currentScroll <= delta) {
-            ref.read(listingsPaginatedProvider(issue).notifier).fetchNext();
-          }
-        }
-        return true;
-      },
-      child: ListView(
-        shrinkWrap: true,
-        children: [
-          ListingStats(
-            issue: issue,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          // const ListingFilters(),
-          // const SizedBox(
-          //   height: 16,
-          // ),
-          ListedItems(
-            issue: issue,
-          ),
-          OnGoingBottomWidget(
-            provider: ref.watch(
-              listingsPaginatedProvider(
-                issue,
-              ),
-            ),
-            sliverWidget: false,
-          ),
-        ],
-      ),
-    );
-  }
-}
 
 class ListingFilters extends ConsumerStatefulWidget {
   const ListingFilters({super.key});
@@ -175,58 +115,5 @@ class _ListingFiltersState extends ConsumerState<ListingFilters> {
         )
       ],
     );
-  }
-}
-
-class ListingStats extends ConsumerWidget {
-  final ComicIssueModel issue;
-  const ListingStats({
-    super.key,
-    required this.issue,
-  });
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final provider = ref.watch(collectionStatsProvider(issue.id));
-
-    return provider.when(data: (collectionStats) {
-      return Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          StatsInfo(
-            title: 'VOLUME',
-            stats: issue.isFreeToRead
-                ? '--'
-                : '${collectionStats?.totalVolume != null ? (collectionStats!.totalVolume / lamportsPerSol).toStringAsFixed(2) : 0}◎',
-          ),
-          StatsInfo(
-            title: 'SUPPLY',
-            stats: issue.isSecondarySaleActive
-                ? '${collectionStats?.supply}'
-                : '--',
-          ),
-          StatsInfo(
-            title: 'LISTED',
-            stats: issue.isFreeToRead
-                ? '--'
-                : '${collectionStats?.itemsListed ?? 0}',
-          ),
-          StatsInfo(
-            title: 'FLOOR',
-            stats: issue.isFreeToRead
-                ? 'FREE'
-                : '${collectionStats?.floorPrice != null ? Formatter.formatLamportPrice(collectionStats!.floorPrice) : '--'}◎',
-            isLastItem: true,
-          ),
-        ],
-      );
-    }, error: (Object error, StackTrace stackTrace) {
-      return const Text('Something went wrong in candy machine stats');
-    }, loading: () {
-      return const Padding(
-        padding: EdgeInsets.symmetric(horizontal: 12, vertical: 8),
-        child: SkeletonRow(),
-      );
-    });
   }
 }
